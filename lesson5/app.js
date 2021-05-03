e8d06e94055f6ba47cc671feb896d79a862fb208
miskosn <sergejlevsna@gmail.com>
2021-05-03 15:18:34 +0300

    clickable and not clickable links added

--- lesson5/app.js
+++ lesson5/app.js
@@ -3,21 +3,25 @@ new Vue({
     data: {
         stock: [
             {   
+                link: "https://www.coca-cola.ua/our-brands/fanta",
                 name: "Orange soda",
                 instock: false
             },
 
             {   
+                link: "https://www.coca-cola.ua/our-brands/sprite",
                 name: "Lemon-lime soda",
                 instock: true
             },
             
             {
+                link: "https://deusex.fandom.com/wiki/Augmentation_canister",
                 name: "nanomachines pod",
                 instock: false
             },
 
             {
+                link: "https://deusex.fandom.com/wiki/Dragon%27s_Tooth_sword_(DX)",
                 name: "Dragon's Tooth nano-blade",
                 instock: true
             }
