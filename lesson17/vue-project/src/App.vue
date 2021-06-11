79b03d5db5744f398c4d2fe03fcdeb35b988793c
miskosn <sergejlevsna@gmail.com>
2021-06-12 01:34:27 +0300

    about slots

---
+++ lesson17/vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <MySlot>
+      <h2 slot="title2">Title iside MySlot</h2>
+      <p slot="text">Text inside MySlot</p>
+    </MySlot>
+  </div>
+</template>
+
+
+<script>
+import MySlot from './components/MySlot.vue'
+
+export default {
+  components: {
+    MySlot,
+  } 
+}
+</script>
+
+<style>
+
+</style>
---
+++ lesson17/vue-project/src/components/MySlot.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+        <slot name="title2"></slot>
+        <h1>This is slot's title!</h1>
+        <slot name="text"></slot>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+ 
+}
+</script>
+
+<style>
+    h1 {
+        color: red;
+        text-shadow: 1px 1px 2px black;
+    }
+</style>
