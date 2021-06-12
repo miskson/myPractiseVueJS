f6c5c43963811c0c956e18931cbff78954b69b1a
miskosn <sergejlevsna@gmail.com>
2021-06-12 22:08:15 +0300

    lesson about props added

---
+++ lesson18/vue-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <List v-bind:listElem = 'listElem'/>
+  </div>
+</template>
+
+<script>
+import List from './components/List';
+
+export default {
+  
+
+  name: 'App',
+  components: {
+    List,
+  },
+  data () {
+    return {
+      listElem: ['elem1', 'elem2', 'elem3', 'elem4', 'elem5'],
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ lesson18/vue-project/src/components/List.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h2 @click="show">List title:</h2>
+        <ul>
+            <li v-for="elem in listElem">{{elem}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    props: {
+        listElem: {
+            type: Array,
+            requiered: false,
+            default: [], 
+        },
+    },
+
+    methods: {
+        show: function(params) {
+            console.log(this.listElem)
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
