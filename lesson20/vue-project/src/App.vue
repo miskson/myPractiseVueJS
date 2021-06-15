0f37b426bcf26d808336dc8529b72724e8aa6a9a
miskosn <sergejlevsna@gmail.com>
2021-06-15 17:56:40 +0300

    lesson about filters

---
+++ lesson20/vue-project/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>{{title | to-uppercase}}</h1>
+    <h3>input affects title. title has to-uppercase global filter.</h3>
+    <input type="text" v-model="title">
+    <h3>this input affects text. text has to-lowercase local filter</h3>
+    <input type="text" v-model="text">
+    <p>{{text | to-lowercase}}</p>
+   
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return{
+      title: 'THE TITILE',
+      text: 'TO LOWER LO LOWER TO LOWER TO LOVER TO LOWWWWER'
+    }
+  },
+
+  filters: {
+    'to-lowercase': function(value) {
+      return value.toLowerCase();
+    }
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
